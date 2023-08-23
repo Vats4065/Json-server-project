e58555aeffee56e0d8716721c1e42589d45e540e
Vats4065 <134689792+Vats4065@users.noreply.github.com>
2023-08-23 17:49:43 +0530

    Links-navbar

---
+++ components/nav.js
@@ -0,0 +1,17 @@
+const nav = () => {
+    return `
+    
+ 
+   <div class="d-flex justify-content-between">
+ <a href="/index.html">Home</a>
+ <a href="/pages/product.html">Product</a>
+   <a href="/pages/cart.html">Cart</a>
+ <a href="/pages/login.html">Login</a>
+<a href="/pages/signup.html">SignUp</a>
+   </div>
+   
+    
+`
+}
+
+export { nav };
---
+++ js/index.js
@@ -0,0 +1,7 @@
+import { nav } from "../components/nav.js"
+
+document.querySelector("nav").innerHTML = nav();
+
+import { link } from "../components/bootstrap.js"
+
+document.getElementById("bootstrap").innerHTML = link();
